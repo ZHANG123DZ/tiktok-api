2ab715bfb1067dd2eebb999ffc1b9c1efd1f7095
zhang <hoangquangthang999@gmail.com>
2025-10-23 23:24:42 +0700

    k

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -1,22 +1,22 @@
-import OpenAI from "openai";
+// import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: process.env.GEMINI_API_KEY,
-  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.GEMINI_API_KEY,
+//   baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+// });
 
-async function send({
-  messages,
-  temperature = 0.7,
-  model = "gemini-2.0-flash",
-}) {
-  const response = await openai.chat.completions.create({
-    model,
-    messages,
-    temperature,
-  });
+// async function send({
+//   messages,
+//   temperature = 0.7,
+//   model = "gemini-2.0-flash",
+// }) {
+//   const response = await openai.chat.completions.create({
+//     model,
+//     messages,
+//     temperature,
+//   });
 
-  return response.choices?.[0]?.message.content || null;
-}
+//   return response.choices?.[0]?.message.content || null;
+// }
 
-export { send };
+// export { send };
